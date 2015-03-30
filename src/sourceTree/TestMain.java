20d97278e1a0526274524628e740327dfeb6b662
SungjinKim <supremejin03@naver.com>
2015-03-30 17:16:57 +0900

    인코딩설정추가

--- src/sourceTree/TestMain.java
+++ src/sourceTree/TestMain.java
@@ -4,7 +4,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("test123");
 		System.out.println("update test");
 		System.out.println("second update test");
 		
